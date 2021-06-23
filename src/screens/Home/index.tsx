3e6a074036317dd26492a41e85f7cf5893319e95
CristianoNunes <cristianonunes99@gmail.com>
2021-06-23 01:47:48 -0300

    Created Screen Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { View, FlatList, Text } from 'react-native';
+
+import { CategorySelect } from '../../components/CategorySelect';
+import ButtonAdd from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+import { ListHeader } from '../../components/ListHeader';
+
+import { styles } from './style';
+
+
+export function Home() {
+  const [category, setCategory ] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    }
+  ]
+
+  function handleCategorySelected(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+  return (
+    <View>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelected}
+        />
+
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+          <FlatList
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Text>{item.guild.name}</Text>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+// Parei em 2h 6 mi e 38 s
